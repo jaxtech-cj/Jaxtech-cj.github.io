8757dcb0d5fee961a8c14e83759e013d4ea7e5f5
Corey Jackson <cjaxsn@gmail.com>
2025-04-06 07:52:57 -0400

    init git

---
+++ vcard/default.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>vCards</title>
+    <style type="text/css">
+        body, a
+        {
+            font-size: 30pt;
+        }
+    </style>
+</head>
+<body>
+   <a href="icaltest1.ics">icaltest1.ics</a><br/><br/>
+   <a href="icaltest2.ics">icaltest2.ics</a><br/><br/>
+   <a href="icaltest3.ics">icaltest3.ics</a><br/><br/>
+   <a href="cj.vcf">cj.vcf</a><br/><br/>
+   <a href="cj.json">cj.json</a><br/><br/>
+   <a href="cjv3.vcf">cjv3.vcf</a><br/><br/>
+   <a href="cjv4.vcf">cjv4.vcf</a><br/><br/>
+   <a href="vcard3.0photobase64.vcf">vcard3.0photobase64.vcf</a><br/><br/>
+   
+</body>
+</html>
+
+
+
+
+
+
+
